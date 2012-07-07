fa30e3e570eead03d79bc6eda06e75340e5615f6
Bartłomiej Kruczyk <bartlomiej.kruczyk@gmail.com>
2012-07-07 13:15:48 +0200

    author reset prototype for single time mode

---
+++ SleepCalculator/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace SleepCalculator
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void PropertyChangedInvoker(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private string _wakeUpTime;
+        public string WakeUpTime 
+        {
+            get { return _wakeUpTime; }
+            set 
+            { 
+                _wakeUpTime = value;
+                PropertyChangedInvoker("GoSleepTime");
+            }
+        }
+
+        public string GoSleepTime
+        {
+            get { return SleepCyclesCalculator.GetGoSleepTime(WakeUpTime); }
+            set
+            {
+                _wakeUpTime = value;
+                PropertyChangedInvoker("GoSleepTime");
+            }
+        }
+    }
+}
---
+++ SleepCalculator/SleepCyclesCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SleepCalculator
+{
+    static class SleepCyclesCalculator
+    {
+        /// <summary>
+        /// Oblicza godzinę pójścia spać dla zadanego czasu pobudki w oparciu o półtoragodzinne cykle snu. 
+        /// </summary>
+        /// <param name="goToSleepTime">
+        /// Czas pójścia spać w formacie HH:mm.
+        /// </param>
+        /// <returns>Trololo</returns>
+        public static string GetGoSleepTime(string wakeUpTime)
+        {
+            DateTime wakeUpDateTime;
+
+            if (DateTime.TryParse(wakeUpTime, out wakeUpDateTime))
+                return wakeUpDateTime.AddHours(-5 * 1.5).ToShortTimeString();
+
+            return "Invalid time format";
+        }
+    }
+}
